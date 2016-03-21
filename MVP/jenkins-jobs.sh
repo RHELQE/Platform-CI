b4bb86842f4bd54a034b868cede1a281ca0db4b3
Linqing Lu <lilu@redhat.com>
2016-03-21 16:48:08 +0800

    [MVP]  initial commit for sample codes

---
+++ MVP/jenkins-jobs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+need_pip()
+{
+    echo "  We need 'pip' to install/update jenkins."
+    echo "  Please run 'easy_install pip' as root to install it."
+    exit 1
+}
+
+upgrade_jjb()
+{
+    which pip > /dev/null || need_pip
+    set -e
+    pip install --quiet --user --upgrade pip
+    pip install --quiet --user --upgrade jenkins-job-builder
+    pip install --quiet --user --upgrade --index-url=http://ci-ops-jenkins-update-site.rhev-ci-vms.eng.rdu2.redhat.com/packages/simple --trusted-host ci-ops-jenkins-update-site.rhev-ci-vms.eng.rdu2.redhat.com jenkins-ci-sidekick
+}
+
+
+if [ $# != 1 ]
+then
+    echo "  To test Jenkins job definition:  $0 test"
+    echo "  To create/update Jenkins jobs:   $0 update"
+    echo "  Please make sure config file 'config.ini' in current dir as well."
+    exit 1
+fi
+
+upgrade_jjb
+
+jenkins-jobs --ignore-cache --conf config.ini $1 .
